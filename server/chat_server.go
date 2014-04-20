af501f5282a85e5e6481060b03e38b5d9c10a995
Thanabodee <thanabodee@osdev.co.th>
2014-04-20 14:37:26 +0700

    reindent chat_server*

--- server/chat_server.go
+++ server/chat_server.go
@@ -1,58 +1,58 @@
 package server
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "errors"
+	"code.google.com/p/go.net/websocket"
+	"errors"
 )
 
 type ChatServer struct {
-    Room map[string][]*Client
+	Room map[string][]*Client
 }
 
 type Client struct {
-    Connection *websocket.Conn
+	Connection *websocket.Conn
 }
 
 var RoomNotExist = errors.New("Room not exist.")
 
 func NewChatServer() *ChatServer {
-    return &ChatServer{Room: make(map[string][]*Client)}
+	return &ChatServer{Room: make(map[string][]*Client)}
 }
 
 func (chatServer *ChatServer) CreateRoom(roomName string, creator *Client) {
-    clients := []*Client{creator}
+	clients := []*Client{creator}
 
-    chatServer.Room[roomName] = clients
+	chatServer.Room[roomName] = clients
 }
 
 func (chatServer *ChatServer) AddClient(c *Client, roomName string) (err error) {
-    if room, exist := chatServer.Room[roomName]; exist {
-        chatServer.Room[roomName] = append(room, c)
-    } else {
-        err = RoomNotExist
-    }
+	if room, exist := chatServer.Room[roomName]; exist {
+		chatServer.Room[roomName] = append(room, c)
+	} else {
+		err = RoomNotExist
+	}
 
-    return
+	return
 }
 
 func (chatServer *ChatServer) FindClientFromRoom(connection *websocket.Conn, roomName string) (*Client, int) {
-    for i, c := range chatServer.Room[roomName] {
-        if c.Connection == connection {
-            return c, i
-        }
-    }
-    return nil, -1
+	for i, c := range chatServer.Room[roomName] {
+		if c.Connection == connection {
+			return c, i
+		}
+	}
+	return nil, -1
 }
 
 func (chatServer *ChatServer) RemoveClientFromRoom(connection *websocket.Conn, roomName string) {
-    _, i := chatServer.FindClientFromRoom(connection, roomName)
-    chatServer.Room[roomName] = append(chatServer.Room[roomName][:i], chatServer.Room[roomName][i+1:]...)
+	_, i := chatServer.FindClientFromRoom(connection, roomName)
+	chatServer.Room[roomName] = append(chatServer.Room[roomName][:i], chatServer.Room[roomName][i+1:]...)
 }
 
 func (chatServer *ChatServer) DeleteRoom(roomName string) {
-    delete(chatServer.Room, roomName)
+	delete(chatServer.Room, roomName)
 }
 
 func NewClient(ws *websocket.Conn) *Client {
-    return &Client{Connection: ws}
+	return &Client{Connection: ws}
 }
--- server/chat_server_test.go
+++ server/chat_server_test.go
@@ -1,141 +1,141 @@
 package server_test
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "github.com/wingyplus/websocket/server"
-    "testing"
+	"code.google.com/p/go.net/websocket"
+	"github.com/wingyplus/websocket/server"
+	"testing"
 )
 
 func TestChatServerCreateRoom(t *testing.T) {
-    var chatServer *server.ChatServer = server.NewChatServer()
+	var chatServer *server.ChatServer = server.NewChatServer()
 
-    chatServer.CreateRoom("Hello", new(server.Client))
+	chatServer.CreateRoom("Hello", new(server.Client))
 
-    if _, ok := chatServer.Room["Hello"]; !ok {
-        t.Error("expect room name Hello but was nil")
-    }
+	if _, ok := chatServer.Room["Hello"]; !ok {
+		t.Error("expect room name Hello but was nil")
+	}
 }
 
 func TestChatServerAddClientToExistingRoom(t *testing.T) {
-    var chatServer *server.ChatServer = server.NewChatServer()
-    chatServer.CreateRoom("Hello", new(server.Client))
+	var chatServer *server.ChatServer = server.NewChatServer()
+	chatServer.CreateRoom("Hello", new(server.Client))
 
-    chatServer.AddClient(new(server.Client), "Hello")
+	chatServer.AddClient(new(server.Client), "Hello")
 
-    v, ok := chatServer.Room["Hello"]
+	v, ok := chatServer.Room["Hello"]
 
-    if !ok {
-        t.Error("expect room name Hello but was nil")
-    }
+	if !ok {
+		t.Error("expect room name Hello but was nil")
+	}
 
-    if len(v) != 2 {
-        t.Error("client not add")
-    }
+	if len(v) != 2 {
+		t.Error("client not add")
+	}
 }
 
 func TestChatServerAddClientToNotExistingRoom(t *testing.T) {
-    var chatServer *server.ChatServer = server.NewChatServer()
+	var chatServer *server.ChatServer = server.NewChatServer()
 
-    err := chatServer.AddClient(new(server.Client), "Hello")
+	err := chatServer.AddClient(new(server.Client), "Hello")
 
-    if err == nil {
-        t.Error("error not raise.")
-    } else {
-        if err.Error() != "Room not exist." {
-            t.Error("room must be not exist.")
-        }
-    }
+	if err == nil {
+		t.Error("error not raise.")
+	} else {
+		if err.Error() != "Room not exist." {
+			t.Error("room must be not exist.")
+		}
+	}
 }
 
 func TestDeleteRoom(t *testing.T) {
-    var chatServer *server.ChatServer = server.NewChatServer()
+	var chatServer *server.ChatServer = server.NewChatServer()
 
-    chatServer.CreateRoom("Hello", new(server.Client))
+	chatServer.CreateRoom("Hello", new(server.Client))
 
-    chatServer.DeleteRoom("Hello")
+	chatServer.DeleteRoom("Hello")
 
-    if _, ok := chatServer.Room["Hello"]; ok {
-        t.Error("expect room name Hello not exist but exist")
-    }
+	if _, ok := chatServer.Room["Hello"]; ok {
+		t.Error("expect room name Hello not exist but exist")
+	}
 }
 
 func TestClient(t *testing.T) {
-    ws := new(websocket.Conn)
-    var c *server.Client = server.NewClient(ws)
+	ws := new(websocket.Conn)
+	var c *server.Client = server.NewClient(ws)
 
-    if c.Connection != ws {
-        t.Errorf("connection address %p is not same ws %p", c.Connection, ws)
-    }
+	if c.Connection != ws {
+		t.Errorf("connection address %p is not same ws %p", c.Connection, ws)
+	}
 }
 
 func TestFindClient(t *testing.T) {
-    connections := []*websocket.Conn{}
+	connections := []*websocket.Conn{}
 
-    for i := 1; i <= 10; i++ {
-        connections = append(connections, new(websocket.Conn))
-    }
+	for i := 1; i <= 10; i++ {
+		connections = append(connections, new(websocket.Conn))
+	}
 
-    chatServer := server.NewChatServer()
-    chatServer.CreateRoom("Hello", server.NewClient(connections[0]))
+	chatServer := server.NewChatServer()
+	chatServer.CreateRoom("Hello", server.NewClient(connections[0]))
 
-    for i := 1; i < 10; i++ {
-        chatServer.AddClient(server.NewClient(connections[i]), "Hello")
-    }
+	for i := 1; i < 10; i++ {
+		chatServer.AddClient(server.NewClient(connections[i]), "Hello")
+	}
 
-    client, index := chatServer.FindClientFromRoom(connections[2], "Hello")
+	client, index := chatServer.FindClientFromRoom(connections[2], "Hello")
 
-    if client.Connection != connections[2] {
-        t.Errorf("connection address %p is not same ws %p", client.Connection, connections[2])
-    }
-    if index != 2 {
-        t.Errorf("expect 2 but was %d", index)
-    }
+	if client.Connection != connections[2] {
+		t.Errorf("connection address %p is not same ws %p", client.Connection, connections[2])
+	}
+	if index != 2 {
+		t.Errorf("expect 2 but was %d", index)
+	}
 
-    client, index = chatServer.FindClientFromRoom(connections[4], "Hello")
+	client, index = chatServer.FindClientFromRoom(connections[4], "Hello")
 
-    if client.Connection != connections[4] {
-        t.Errorf("connection address %p is not same ws %p", client.Connection, connections[4])
-    }
-    if index != 4 {
-        t.Errorf("expect 4 but was %d", index)
-    }
+	if client.Connection != connections[4] {
+		t.Errorf("connection address %p is not same ws %p", client.Connection, connections[4])
+	}
+	if index != 4 {
+		t.Errorf("expect 4 but was %d", index)
+	}
 
-    client, index = chatServer.FindClientFromRoom(new(websocket.Conn), "Hello")
+	client, index = chatServer.FindClientFromRoom(new(websocket.Conn), "Hello")
 
-    if client != nil {
-        t.Errorf("expect nil but was %v", client)
-    }
-    if index != -1 {
-        t.Errorf("expect -1 but was %d", index)
-    }
+	if client != nil {
+		t.Errorf("expect nil but was %v", client)
+	}
+	if index != -1 {
+		t.Errorf("expect -1 but was %d", index)
+	}
 }
 
 func TestRemoveClientFromRoom(t *testing.T) {
-    connections := []*websocket.Conn{}
+	connections := []*websocket.Conn{}
 
-    for i := 1; i <= 10; i++ {
-        connections = append(connections, new(websocket.Conn))
-    }
+	for i := 1; i <= 10; i++ {
+		connections = append(connections, new(websocket.Conn))
+	}
 
-    chatServer := server.NewChatServer()
-    chatServer.CreateRoom("Hello", server.NewClient(connections[0]))
+	chatServer := server.NewChatServer()
+	chatServer.CreateRoom("Hello", server.NewClient(connections[0]))
 
-    for i := 1; i < 10; i++ {
-        chatServer.AddClient(server.NewClient(connections[i]), "Hello")
-    }
+	for i := 1; i < 10; i++ {
+		chatServer.AddClient(server.NewClient(connections[i]), "Hello")
+	}
 
-    chatServer.RemoveClientFromRoom(connections[4], "Hello")
+	chatServer.RemoveClientFromRoom(connections[4], "Hello")
 
-    if len(chatServer.Room["Hello"]) != 9 {
-        t.Errorf("expect 9 but was %d", len(chatServer.Room["Hello"]))
-    }
+	if len(chatServer.Room["Hello"]) != 9 {
+		t.Errorf("expect 9 but was %d", len(chatServer.Room["Hello"]))
+	}
 
-    c, i := chatServer.FindClientFromRoom(connections[4], "Hello")
+	c, i := chatServer.FindClientFromRoom(connections[4], "Hello")
 
-    if c != nil {
-        t.Errorf("expect nil but was %v", c)
-    }
-    if i != -1 {
-        t.Errorf("expect -1 but was %d", i)
-    }
+	if c != nil {
+		t.Errorf("expect nil but was %v", c)
+	}
+	if i != -1 {
+		t.Errorf("expect -1 but was %d", i)
+	}
 }
